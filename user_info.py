26ee40c096625cffb228810cc7c77ef17304eff8
niushaolong <niushaolong@126.com>
2024-06-12 21:41:50 +0800

    删除cookie

--- user_info.py
+++ user_info.py
@@ -1,5 +1,5 @@
 STR_COOKIE_MAIN = \
-"mstuid=1689681055043_8460; Hm_lvt_c3e3e8b3ea48955284516b186acf0f4e=1698320891; uLocale=zh_CN; iplocale=zh_CN; userId=1485538246; i.mi.com_istrudev=true; i.mi.com_isvalid_servicetoken=true; i.mi.com_ph=B2HvhV75cQtH8+83kH5p8g==; serviceToken=jNa2f6UlbQ0iiMIayebNzXjfA/5lKIM4krT0g9uhqh80opAjtN38QN8Aj+oPd1qf52EDCl8i/VgpTKChJKYOs1CttaGSSTQJk8CaasyFPyzTNwP94r9cj9KVMDAyYalrFWoutbD4AWQfxb7K7xxbSrg2O133zQDmRx7TGT6cJ9sk4AjJKr+Jtdf6Tkf013w43d31KuRJAvzkZMFWBwe/G95IgDbc2d7TGK9kxxMHaxrGMWB7V3tHkfA3p/OEr1h/dTokwd2INSL5/p21BDFTiU5WFXFty3sXyyTGEo4Km9Oqy0NLghS3YDAETc7beHG8YWfHMq2ilprNzhKT1sGDTvzyOUYktoGj/UqA7/SNFxljfeJLvYxNNDuOkcUSdjG2/ltQvHzjU37/TRYpQE8jfg==; i.mi.com_slh=OwSgCQY7hjfmc9kHIbkPEdP9F4g="
+""
 STR_COOKIE_EVERY_NOTE = STR_COOKIE_MAIN
 URL_GET_NOTES = 'https://i.mi.com/note/full/page/?limit=200'
 URL_GET_NOTE = 'https://i.mi.com/note/note/'
